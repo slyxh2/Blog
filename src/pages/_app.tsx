bc07bb9bd13d89d0be49925a51be967d5766e4ca
slyxh2 <750772121@qq.com>
2023-05-16 14:52:19 -0400

    add ErrorBoundary

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import type { AppProps } from 'next/app'
 import './style/global.css'
 import './style/navbar.css'
+import ErrorBoundary from '../components/ErrorBoundary';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    // Wrap the Component prop with ErrorBoundary component
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
 }
